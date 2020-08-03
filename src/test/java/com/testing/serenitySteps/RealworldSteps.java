833ee6ff3f6af9e3de7837527f0daaaf97e77d3e
marinabitard99 <elfen_lied2@inbox.lv>
2020-08-03 15:06:06 +0300

    task done

--- src/test/java/com/testing/serenitySteps/RealworldSteps.java
+++ src/test/java/com/testing/serenitySteps/RealworldSteps.java
@@ -17,7 +17,8 @@ public class RealworldSteps extends BaseSteps {
   private final static String _API_USERS_ = "/api/users/";
   private final static String _API_ARTICLES_ = "/api/articles/";
   // Find endpoint for API login
-  private final static String _API_USERS_LOGIN_ = null;
+  private final static String _API_USER_ = "/api/user/";
+  private final static String _API_USERS_LOGIN_ = "/api/users/login/";
 
   @Steps
   RealworldSteps realworldSteps;
@@ -59,6 +60,38 @@ public class RealworldSteps extends BaseSteps {
     }
   }
 
+  @Step
+  public static void updateSettings(DataTable dataTable) throws IOException {
+    sendRequestWithBodyJson(PUT, _API_USER_, createBody(dataTable));
+
+
+  }
+
+    @Step
+    public static void createPost(DataTable dataTable) throws IOException {
+      sendRequestWithBodyJson(POST, _API_ARTICLES_, createBody(dataTable));
+    }
+
+    @Step
+    public static void deletePost() {
+      sendRequest(DELETE ,_API_ARTICLES_ + sessionVariableCalled("slug"));
+    }
+
+      @Step
+      public static void modifyPost(DataTable dataTable) throws IOException {
+        sendRequestWithBodyJson(PUT, _API_ARTICLES_ + sessionVariableCalled("slug"), createBody(dataTable));
+
+      }
+
+  @Step
+  public static void addComment(DataTable dataTable) throws  IOException{
+    sendRequestWithBodyJson(POST, addCommentEndpoint("slug"), createBody(dataTable));
+  }
+
+  @Step
+  public static void  deleteComment(){
+    sendRequest(DELETE, addCommentEndpoint("slug") + "/" + sessionVariableCalled("comment_id"));
+  }
 
   // Private
 
